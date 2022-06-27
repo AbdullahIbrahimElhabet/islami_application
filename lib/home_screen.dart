abd9e1e805340ad3ffd7c335546ebed0a027862f
Abdullah Elhabet <elhabetabdullah@gmail.com>
2022-06-27 02:45:45 +0200

    created home screen and sura details screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:islami_refresh/hadeth_tab.dart';
+import 'package:islami_refresh/quran/quran_tab.dart';
+import 'package:islami_refresh/radio_tab.dart';
+import 'package:islami_refresh/sebha_tab.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const String screenName = "home";
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        Image.asset("assets/images/home_screen.png"),
+        Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text("Islami", style: Theme.of(context).textTheme.headline1),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: currentIndex,
+            onTap: (int index) {
+              currentIndex = index;
+              setState(() {});
+            },
+            items: [
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage("assets/images/ic_quran.png")),
+                  label: "Quran",
+                  backgroundColor: Theme.of(context).primaryColor),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage("assets/images/ic_hadeth.png")),
+                  label: "Hadeeth",
+                  backgroundColor: Theme.of(context).primaryColor),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage("assets/images/ic_sebha.png")),
+                  label: "Sebha",
+                  backgroundColor: Theme.of(context).primaryColor),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage("assets/images/ic_radio.png")),
+                  label: "Radio",
+                  backgroundColor: Theme.of(context).primaryColor),
+            ],
+          ),
+          body: tabs[currentIndex],
+        )
+      ],
+    );
   }
+
+  List<Widget> tabs = [QuranTab(), HadethTab(), SebhaTab(), RadioTab()];
 }
---
+++ lib/quran/sura_details.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami_refresh/quran/sura_details_item.dart';
+
+class SuraDetails extends StatefulWidget {
+  static const String screenName = "suradie";
+
+  @override
+  State<SuraDetails> createState() => _SuraDetailsState();
+}
+
+class _SuraDetailsState extends State<SuraDetails> {
+  List<String> verses = [];
+
+  @override
+  Widget build(BuildContext context) {
+    var args = ModalRoute.of(context)?.settings.arguments as SuraDetailsArg;
+    if (verses.isEmpty) loadFile(args.index);
+    return Stack(
+      children: [
+        Image.asset("assets/images/home_screen.png"),
+        Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text(
+              args.suranName,
+              style: Theme.of(context).textTheme.headline1,
+            ),
+          ),
+          body: verses.length == 0
+              ? Center(child: CircularProgressIndicator())
+              : ListView.separated(
+                  itemCount: verses.length,
+                  separatorBuilder: (context, index) {
+                    return Container(
+                      height: 1,
+                      color: Theme.of(context).primaryColor,
+                      margin: EdgeInsets.symmetric(horizontal: 25),
+                    );
+                  },
+                  itemBuilder: (context, index) {
+                    return SuraDetailsItem(verses[index], index + 1);
+                  }),
+        )
+      ],
+    );
+  }
+
+  void loadFile(int index) async {
+    String content =
+        await rootBundle.loadString("assets/files/${index + 1}.txt");
+    List<String> lines = content.trim().split("\n");
+    verses = lines;
+    print(verses);
+    setState(() {});
+  }
+}
+
+class SuraDetailsArg {
+  String suranName;
+  int index;
+
+  SuraDetailsArg(this.suranName, this.index);
+}
