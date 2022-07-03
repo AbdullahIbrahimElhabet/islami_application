687d3faac821c0dc8bb98f0cf480b879286c226d
Abdullah Elhabet <elhabetabdullah@gmail.com>
2022-07-03 19:24:35 +0200

    using shared preferences to save state program

--- lib/provider/language_provider.dart
+++ lib/provider/language_provider.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LanguageProvider extends ChangeNotifier {
   String baseLanguage = "en";
 
-  void changeLanguage(String newLanguage) {
+  void changeLanguage(String newLanguage) async {
     baseLanguage = newLanguage;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("lang", newLanguage);
     notifyListeners();
   }
 }
